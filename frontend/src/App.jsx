80a10e11706c3dfedbaeba0c9eaef6a1833e4497
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-05-15 14:13:55 +0300

    created register page with labeled inputs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./Pages/LoginPage/LoginPage.jsx";
 import {Route, Routes} from "react-router-dom";
 import WelcomePage from "./Pages/WelcomePage/WelcomePage.jsx";
 import HeaderComponent from "./Components/HeaderComponent/HeaderComponent.jsx";
+import RegisterPage from "./Pages/RegisterPage/RegisterPage.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<WelcomePage/>}/>
                 <Route path='/login' element={<LoginPage/>}/>
+                <Route path='/register' element={<RegisterPage/>}/>
             </Routes>
         </>
 
